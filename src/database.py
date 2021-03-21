bf860def192ac0f0b55d8f0fa89144d786a736dd
sridharblue21 <sridharblue95@gmail.com>
2021-03-21 23:55:00 +0530

    added .env

--- src/database.py
+++ src/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from . import utils
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgres://postgres:postgres@localhost:5432/goaly"
+SQLALCHEMY_DATABASE_URL = utils.get_settings().SQLALCHEMY_DB_URL
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={}
