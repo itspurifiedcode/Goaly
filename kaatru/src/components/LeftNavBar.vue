890bbf9aec81686dc4b1d62090ae158361dde09e
karthickmerk <karthickmerk@outlook.com>
2021-04-15 15:30:59 +0530

    ui refactor and api connection

--- kaatru/src/components/LeftNavBar.vue
+++ kaatru/src/components/LeftNavBar.vue
@@ -1,51 +1,68 @@
 <template>
 
-  
-<div class="p-8  my-10 bg-white border-r-2 border-light-blue-200 ">
-      <span class="flex"> 
-          <svg class="mt-2" width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect x="1.5" y="1.5" width="14" height="14" rx="4.5" stroke="#198CFF" stroke-width="3"/><h3 class="font-medium text-3xl text-blue-500 py-10 px-3 flex-none">Projects</h3>
-          </svg>
-          <h1 class="font-medium text-blue-600 px-2 text-2xl"> Projects</h1>
-      </span>
-          <div>
-            <a href="/"><h1 class="font-bold text-blue-600 p-3 mt-3 px-20 text-2xl bg-blue-200 rounded-3xl">Contributers</h1></a> 
-            <ul class="px-20 py-5 font-medium ">
-              <li class="py-2 text-gray-600	">Murali</li>
-              <li class="py-2 text-gray-600	">Sridher</li>
-              <li class="py-2 text-gray-600	">Karthick</li>
-            </ul>
-          </div>
-      <span class="flex"> <svg class="mt-2" width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect x="1.5" y="1.5" width="14" height="14" rx="4.5" stroke="#42CF58" stroke-width="3"/><h3 class="font-medium text-3xl text-blue-500 py-10 px-3 flex-none">Projects</h3>
-      </svg>
-      <h1 class="font-medium text-green-600 px-2 text-2xl"> Projects</h1></span>
-          <div>
-            <h1 class="font-bold text-green-600 p-3 mt-3 px-20 text-2xl bg-green-200 rounded-3xl">Contributers</h1>  
-            <ul class="px-20 py-5 font-medium ">
-              <li class="py-2 text-gray-600	">Murali</li>
-              <li class="py-2 text-gray-600	">Sridher</li>
-              <li class="py-2 text-gray-600	">Karthick</li>
-            </ul>
-          </div>
-      <span class="flex"> <svg class="mt-2" width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect x="1.5" y="1.5" width="14" height="14" rx="4.5" stroke="#FF9519" stroke-width="3"/><h3 class="font-medium text-3xl text-blue-500 py-10 px-3 flex-none">Projects</h3>
-      </svg>
-      <h1 class="font-medium text-yellow-600 px-2 text-2xl"> Projects</h1></span>
-          <div>
-            <h1 class="font-bold text-yellow-600 p-3 mt-3 px-20 text-2xl bg-yellow-200 rounded-3xl">Contributers</h1>  
-            <ul class="px-20 py-5 font-medium ">
-              <li class="py-2 text-gray-600	">Murali</li>
-              <li class="py-2 text-gray-600	">Sridher</li>
-              <li class="py-2 text-gray-600	">Karthick</li>
-            </ul>
-          </div>
+<div class="bg-gray-100">
+<!-- card start-->
+ <div >
+  <div class="p-6 max-w-sm m-4 bg-white rounded-xl shadow-md flex items-center space-x-4 ">
+  <div class="flex-shrink-0">
+    <img class="h-12 w-12" src="../assets/img/chaticon.svg" alt="ChitChat Logo">
   </div>
+  <div>
+    <div class="text-2xl font-medium text-black font-bold">{{posts[0].name}}</div>
+    <p class="text-gray-400 text-sm">{{posts[0].description}}</p>
+    <small>Created: <strong>15-04-2020</strong> Last Change: <strong>15-04-2021</strong> </small>
+  </div>
+</div>
+</div>
+ <!-- card start end-->
+<!-- card start-->
+
+  <div class="p-6 max-w-sm m-4 bg-white rounded-xl shadow-md flex items-center space-x-4 ">
+  <div class="flex-shrink-0">
+    <img class="h-12 w-12" src="../assets/img/chaticon green.svg" alt="ChitChat Logo">
+  </div>
+  <div>
+    <div class="text-2xl font-medium text-black font-bold">{{posts[1].name}}</div>
+    <p class="text-gray-400 text-sm">{{posts[1].description}}
+</p>
+    <small>Created: <strong>15-04-2020</strong> Last Change: <strong>15-04-2021</strong> </small>
+  </div>
+</div>
+
+ <!-- card start end-->
+
+ </div>
 
 </template>
 
+
 <script>
+import axios from 'axios';
+
 export default {
-    name:'LeftNavBar'
+  name:'LeftNavBar',
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  async created() {
+    try {
+      const response = await axios.get(`/api`)
+      this.posts = response.data.goals
+    } catch (e) {
+      this.errors.push(e)
+    }
+  }
 }
 </script>
+
+<style>
+.card{
+  width: 450px;
+  height: 300px;
+}
+</style>
