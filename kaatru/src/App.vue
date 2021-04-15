7126b28c97f84963d71c53dd238022be844f0284
murali <muraliz2>
2021-04-15 13:40:12 +0530

    🐛 FIX: 📦 NEW: fixed side nav

--- kaatru/src/App.vue
+++ kaatru/src/App.vue
@@ -1,41 +1,43 @@
 <template>
-   <div class="flex flex-row h-screen">
-    <LeftLogoBar/>
-    <LeftNavBar/>
-    <MainWindow/>
-  </div>
+  <div class="flex mb-4 relative ">
+    <div class="hidden md:block w-20 p-4 fixed h-full bg-white z-10 bg-blue-600 ">
+      <LeftLogoBar />
+    </div>
 
+    <div class="flex w-full relative bg-white">
+      <LeftNavBar />
+      <MainWindow />
+    </div>
+  </div>
 </template>
 
 <script>
-import LeftNavBar from './components/LeftNavBar'
-import LeftLogoBar from './components/leftLogoBar'
-import MainWindow from './components/MainWindow.vue'
+import LeftNavBar from "./components/LeftNavBar";
+import LeftLogoBar from "./components/leftLogoBar";
+import MainWindow from "./components/MainWindow.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     LeftNavBar,
     LeftLogoBar,
-    MainWindow
-  }
-}
+    MainWindow,
+  },
+};
 </script>
 
 <style>
-
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 #app {
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #d7d7d8;
   margin-top: 60px;
 }
-body{
-  font-family: 'Poppins', sans-serif;  
+body {
+  font-family: "Poppins", sans-serif;
 }
-.logo{
+.logo {
   color: #000;
 }
-
 </style>
--- kaatru/src/components/leftLogoBar.vue
+++ kaatru/src/components/leftLogoBar.vue
@@ -1,19 +1,27 @@
 <template>
-    <div class="flex flex-col justify-between items-center p-4 bg-blue-600">
-    <div class="flex flex-col space-y-4">
-      <a href="/home"><h1 class="rounded-full w-8 h-9 text-white	font-bold text-5xl">G</h1></a>
-      <a href="/home"><p class="logo text-white text-1">Goaly</p></a>
+  <!-- Sidebar -->
+  <div >
+    <!-- Absolute Frame for Sidebar Content and Drawer    -->
+      <!-- Sidebar Content -->
+      <div
+        class="items-top justify-center"
+      >
+        <div class="flex flex-col space-y-4">
+          <a href="/home"
+            ><h1 class="rounded-full w-8 h-9 text-white font-bold text-5xl">
+              G
+            </h1></a
+          >
+          <a href="/home"><p class="logo text-white text-1">Goaly</p></a>
+        </div>
     </div>
   </div>
-
 </template>
-
 <script>
 export default {
-    name:'LogoBar'
-}
+  name: "LogoBar",
+};
 </script>
 
 <style>
-
 </style>
