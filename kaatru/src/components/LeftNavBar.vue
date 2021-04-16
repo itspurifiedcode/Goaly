a895a3754e9b95d6e73ce30000b3346af1b4f267
karthickmerk <karthickmerk@outlook.com>
2021-04-16 17:12:16 +0530

    bat added

--- kaatru/src/components/LeftNavBar.vue
+++ kaatru/src/components/LeftNavBar.vue
@@ -11,6 +11,10 @@
     <div class="text-2xl font-medium text-black font-bold">{{goal.name}}</div>
     <p class="text-gray-400 text-sm">{{goal.description}}</p>
     <small>Created: <strong>15-04-2020</strong> Last Change: <strong>15-04-2021</strong> </small>
+       <div class="bg-gray-200 dark:bg-black rounded-full overflow-hidden mt-3">
+      <div class="bg-lime-500 bg-green-400 w-1/4 h-1.5" role="progressbar" aria-valuenow="1456" aria-valuemin="0" aria-valuemax="4550"></div>
+    </div>
+    
   </div>
 </div>
 </div>
