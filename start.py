7cb80f9157578bc03e82e980accdbf2fdb318c7f
murali <muraliz2>
2021-04-18 17:06:41 +0530

    👌 IMPROVE: refactor start script

--- start.py
+++ start.py
@@ -4,15 +4,8 @@ import subprocess
 import sys
 import webbrowser
 
-
-
 rootPath = os.path.dirname(os.path.abspath(__file__))
-print("initial path", rootPath)
 currentOS = platform.system()
-
-def my_subprocess(command,c='C:\here'):
-     subprocess.call(['C:\\Windows\\System32\\Notepad.exe', 'C:\\openThisfile.txt'])
-
 
 if currentOS == "Linux":
     print("Linux")
@@ -21,8 +14,6 @@ else:
     aagayamPath = f"{rootPath}\\aagayam"
     print(aagayamPath)
     if os.path.exists(aagayamPath) and os.path.isdir(aagayamPath):
-        os.chdir(aagayamPath)
-        os.chdir(f"{aagayamPath}\\env\\Scripts")
         command = f"cd {aagayamPath}/env/Scripts & activate & cd ../../ && uvicorn src.server:app --host 0.0.0.0 --port 8000 --reload"
         subprocess.Popen(f'cmd.exe /K {command}', creationflags=subprocess.CREATE_NEW_CONSOLE) 
         webbrowser.open('http://localhost:8000', new=2)
